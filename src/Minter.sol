9dae343b2d293f51684c0988d491c2dabaefd49a
0xnavigator <0xnavigator@proton.me>
2023-02-08 09:22:18 +0100

    Fixed redundant Math.min check

--- src/Minter.sol
+++ src/Minter.sol
@@ -4,13 +4,11 @@ pragma solidity ^0.8.13;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/utils/math/Math.sol';
 
 import './interfaces/IMinter.sol';
 
 contract Minter is IMinter, ERC20, Ownable {
   using SafeERC20 for ERC20;
-  using Math for uint;
 
   struct Investor {
     uint cap;
@@ -125,8 +123,6 @@ contract Minter is IMinter, ERC20, Ownable {
     uint _currentAmount,
     uint _maximumAmount
   ) internal pure returns (uint _maximumAllowed) {
-    _maximumAllowed = _inputAmount + _currentAmount > _maximumAmount
-      ? Math.min(_maximumAmount - _currentAmount, _inputAmount)
-      : _inputAmount;
+    _maximumAllowed = _inputAmount + _currentAmount > _maximumAmount ? _maximumAmount - _currentAmount : _inputAmount;
   }
 }
